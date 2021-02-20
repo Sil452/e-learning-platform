fc2ad27c84ba0036fba2af4bad73ddf206ccc247
silvia <silviapintus.93@gmail.com>
2021-02-20 19:51:35 +0000

    courses table created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "static_pages#landing_page"
-  get "privacy_policy", to: "static_pages#privacy_policy"
+    root "static_pages#landing_page"
+    
+    get "privacy_policy", to: "static_pages#privacy_policy"
+    resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
