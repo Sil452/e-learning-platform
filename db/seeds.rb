e8012c2860f3da07a2e924d1ddae7e35373b0268
silvia <silviapintus.93@gmail.com>
2021-02-21 18:32:17 +0000

    relationship between user and courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: "sp@gmail.com", password: "hellohello", password_confirmation: "hellohello")
+
 30.times do
   Course.create! ([{
     title: Faker::Educator.course_name,
-    description: Faker::Quote.famous_last_words
+    description: Faker::Quote.famous_last_words,
+    user_id: User.first.id
   }])
 end
