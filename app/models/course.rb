eb6960a5e6575411aefe5f0c30c65ca45d15a450
silvia <silviapintus.93@gmail.com>
2021-02-24 18:00:30 +0000

    friendly_id gem

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,9 @@ class Course < ApplicationRecord
   validates :title,  presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  
   belongs_to :user
   
   def to_s
