6db1581cbef3e9eb4840c74ea575b1897c9b44fa
silvia <silviapintus.93@gmail.com>
2021-02-24 19:13:13 +0000

    users controller and view + user card

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     
     get "privacy_policy", to: "static_pages#privacy_policy"
     resources :courses
+    resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
