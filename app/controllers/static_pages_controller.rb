d10a12eb5f644be16213513d0d4b5bf4cf954f53
silvia <silviapintus.93@gmail.com>
2021-03-07 11:38:06 +0000

    fixed search problem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,6 @@ class StaticPagesController < ApplicationController
   def landing_page
     @courses = Course.all.limit(3)
     @latest_couses = Course.all.limit(3).order(created_at: :desc)
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
   end
 end
