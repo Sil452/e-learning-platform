206705b7a3b07aa0c4802e2bff85020ac30819a8
silvia <silviapintus.93@gmail.com>
2021-04-06 18:28:01 +0000

    nested routes for lessons in courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :lessons
   devise_for :users
     root "static_pages#landing_page"
     
     get "static_pages/activity"
     resources :users, only: [:index, :edit, :show, :update]
-    resources :courses
+    resources :courses do
+      resources :lessons
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
